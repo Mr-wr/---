4c252ed15c9b6851cb25a86654a47a07843b2257
mr-wr <1434687824@qq.com>
2022-08-18 13:55:08 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,11 @@
+// let a = ['asdf', 'sa1df', 'sda3f']
+// a.splice(1, 1)
+// console.log('log - a ', a)
+// const t = {
+//   a: function () {
+//     setTimeout(function () {
+//       console.log(this)
+//     }, 1000)
+//   },
+// }
+// t.a()
